9aeea7f0fd818117f7153365099fb4f15a80774e
Laurin Brittain <brittain.laurin@outlook.com>
2024-11-20 17:08:57 -0600

    Update frontend build and deploy changes

--- server.js
+++ server.js
@@ -1,33 +1,10 @@
+const path = require('path');
 const express = require('express');
-const cors = require('cors');
-const path = require('path'); // Required to serve static files
-const connectDB = require('./config/db'); // Corrected path for MongoDB connection
-const authRoutes = require('./routes/authRoutes');
-const leadsRoute = require('./routes/leads'); // Updated path and name
-
-require('dotenv').config(); // Load environment variables
-
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-// Middleware
-app.use(cors());
-app.use(express.json()); // Built-in body parser
-
-// Connect to MongoDB
-connectDB(); // Calls the function from db.js to connect to MongoDB
-
-// Minimal API Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/leads', leadsRoute);
-
-// Root route for API
-app.get('/api', (req, res) => {
-    res.send('Welcome to the Document Digitization API!');
-});
-
-// Serve React Frontend
-const buildPath = path.join(__dirname, 'build'); // Adjusted path for the React build directory
+// Serve React frontend from my-api/build
+const buildPath = path.join(__dirname, 'build');
 app.use(express.static(buildPath));
 
 // Catch-all route for React
