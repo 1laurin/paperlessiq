8022553be4f071ac1e7da837d3ccc03d71cc8f66
Laurin Brittain <brittain.laurin@outlook.com>
2024-11-25 14:39:44 -0600

    Update frontend build and deploy changes 11-25-239pm

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5001;
 // Middleware to parse JSON (if your API requires it)
 app.use(express.json());
 
-// API routes
+//API routes
 app.use('/api', (req, res) => {
     res.json({ message: 'API is working!' });
 });
@@ -22,5 +22,7 @@ app.get('*', (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    // Log a message with the correct production URL if applicable
+    const baseURL = process.env.NODE_ENV === 'production' ? 'https://paperlessiq.onrender.com' : `http://localhost:${PORT}`;
+    console.log(`Server running on ${baseURL}`);
 });
