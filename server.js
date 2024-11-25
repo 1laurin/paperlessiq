795adf1903e46e90e0ba396910c1a0977ebf36cd
Laurin Brittain <brittain.laurin@outlook.com>
2024-11-25 15:11:14 -0600

    Update frontend build and deploy changes 11-25-311pm

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const path = require('path');
 const express = require('express');
+const connectDB = require('config/db');
 const app = express();
 const PORT = process.env.PORT || 5001;
+
+// Connect to MongoDB
+connectDB();
 
 // Middleware to parse JSON (if your API requires it)
 app.use(express.json());
