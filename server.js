596663784541bf50d55bb011522c8a92b0ca15c7
Laurin Brittain <brittain.laurin@outlook.com>
2024-11-25 15:18:26 -0600

    Update frontend build and deploy changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-const connectDB = require('config/db');
+const connectDB = require('./config/db');
 const app = express();
 const PORT = process.env.PORT || 5001;
 
